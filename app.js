8879c7fb3df6b09d3b78fbc5bafb2288f6d237a1
001AJD <ajinkyadhomne.27@gmail.com>
2022-02-23 10:18:34 +0530

    updated db functions

--- app.js
+++ app.js
@@ -1,28 +1,23 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const bodyParser = require('body-parser')
-const mongoConnection = require('./mongo/dbConnection')
+const mongoConnection = require('./mongo/dbConnection');
 
+// routers
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var employeeRouter = require('./routes/employee');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-mongoConnection.createConnection();
+mongoConnection.createConnection(); // opens a DB connection
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/employee', usersRouter);
+app.use('/employee', employeeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- mongo/dbConnection.js
+++ mongo/dbConnection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const connectionURI = 'Connection string here';
+require('dotenv').config(); // loads .env file into process env
+const connectionURI = process.env.MONGO_CONNECTION_STRING;
 exports.createConnection = ()=>{
 	mongoose.connect(connectionURI,(err)=>{
 		if(err)
